32f1184fa39a90ad70edf64e363af58f8909500b
Vladislav <Vladislav>
2016-10-10 16:00:32 +0300

    Окно подготовка без логики

---
+++ Fresnel/SetupFresnel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresnel
+{
+    class SetupFresnel
+    {
+        public double StepCalculation(double freq, double height, double width, double dist, double rpArea)
+        {
+            double step = freq + height + width + dist + rpArea;
+            return step;
+        }
+    }
+}
